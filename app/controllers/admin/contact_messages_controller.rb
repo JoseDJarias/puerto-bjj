d72b91609b6ae00c456b4adf0016bd7a7dfc317a
JoseDJarias <arias_da044@hotmail.com>
2025-05-07 21:11:26 -0600

    Contact admin methods

--- app/controllers/admin/contact_messages_controller.rb
+++ app/controllers/admin/contact_messages_controller.rb
@@ -1,7 +1,9 @@
 class Admin::ContactMessagesController < ApplicationController
   def index
+    @messages = ContactMessage.order(created_at: :desc)
   end
 
   def show
+    @message = ContactMessage.find(params[:id])
   end
 end
